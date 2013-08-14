9d8f9ba4cac823ebcffe16f97ad46ae0e5a25fec
Kirill Berezin <enelar@develop-project.ru>
2013-08-14 07:58:55 +0400

    Server gets messages and answers

--- source/server/server_module.h
+++ source/server/server_module.h
@@ -1,9 +1,31 @@
 #include "../../ed/source/ed/kit/module.h"
 
+#include <string>
+#include <iostream>
+
+enum MY_EVENTS
+{
+  CHAT_MESSAGE_EVENT
+};
+
 struct chat_server_module : public ed::module
 {
   chat_server_module( ed::gateway &_gw ) : module("CHAT_SERVER", _gw)
-  {}
+  {
+    RegisterEvent(TOSTRING(CHAT_MESSAGE_EVENT), CHAT_MESSAGE_EVENT);
+
+    ed::event_source es;
+    es.event = CHAT_MESSAGE_EVENT;
+
+    RegisterPostHandler(&chat_server_module::OnMessage, es);
+  }
+  void OnMessage( const ed::event_context<std::string> &a )
+  {
+    throw_assert(a.payload);
+    std::cout << "RECV:" << *a.payload << std::endl;
+    SendEvent(CHAT_MESSAGE_EVENT, *a.payload); 
+  }
+
   ~chat_server_module()
   {
     UnregisterHandlers(this);
